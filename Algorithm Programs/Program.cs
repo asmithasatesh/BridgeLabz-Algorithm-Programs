9219387287de6b037f97e18c0886a87c46ea57e4
asmithasatesh <asmithasatesh@gmail.com>
2021-07-12 12:18:32 +0530

    [Asmitha]Initial commit with Welcome Message

---
+++ Algorithm Programs/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Algorithm_Programs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome To Algorithm Programs!");
+        }
+    }
+}
