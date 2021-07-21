2520238c68625bb3c082aae2ca69b724177bb120
asmithasatesh <asmithasatesh@gmail.com>
2021-07-21 17:39:29 +0530

    [Asmitha]Calculate Permutation of a string

--- Algorithm Programs/Program.cs
+++ Algorithm Programs/Program.cs
@@ -8,6 +8,10 @@ namespace Algorithm_Programs
         {
             Console.WriteLine("Welcome To Algorithm Programs!");
             Console.WriteLine("Enter 1-to Calculate Permutation of a string");
+
+            //Creating generics Object for generic Class
+
+
             int ch = Convert.ToInt32(Console.ReadLine());
             switch(ch)
             {
@@ -17,17 +21,6 @@ namespace Algorithm_Programs
                     int n = str.Length;
                     Console.WriteLine("Permutation through Recursion Method");
                     Permutation.RecursivePermutation(str, 0, n - 1);
-                    Console.WriteLine("\nPermutation through Iteration Method");
-                    Permutation.IterativePermutation(str, n);
-                    if(Permutation.recursiveList == Permutation.iterativeList)
-                    {
-                        Console.WriteLine("Both recursive and Iterative Method yield same array!");
-                    }
-                    else
-                    {
-                        Console.WriteLine("Both recursive and Iterative Method yield different array!");
-
-                    }
                     break;
             }
            
